fa8948911289a4caf38ade7e8ea1c63a94d241fd
arsen <arsen@mail.ru>
2020-01-10 16:47:35 +0300

    batch update

--- gates.py
+++ gates.py
@@ -2,14 +2,14 @@ import numpy as np
 from scipy.linalg import expm
 import math
 
-def destroy_operator(n_photons):
-    matrix = np.zeros((n_photons, n_photons))
+def destroy_operator(batch_size, n_photons):
+    matrix = np.zeros((batch_size, n_photons, n_photons))
     for i in range(1,n_photons):
-        matrix[i-1,i] = np.sqrt(i)
+        matrix[:,i-1,i] = np.sqrt(i)
     return matrix
 
-def create_operator(n_photons):
-    return destroy_operator(n_photons).conj().T
+def create_operator(batch_size, n_photons):
+    return destroy_operator(batch_size, n_photons).conj().transpose(0,2,1)
 
 
 def S_gate(r, phi, n_photons):
@@ -17,55 +17,90 @@ def S_gate(r, phi, n_photons):
     Squeezing gate
     """
     z = r*np.exp(phi*1j)
-    eq = z.conj()*np.linalg.matrix_power(destroy_operator(n_photons),2) - z*np.linalg.matrix_power(create_operator(n_photons),2)
-    return expm(0.5*eq)
+    batch_size = z.shape[0]
+    z = z.reshape(batch_size, 1, 1)
+    eq = np.multiply(z.conj(), np.linalg.matrix_power(destroy_operator(batch_size, n_photons),2)) -\
+         np.multiply(z, np.linalg.matrix_power(create_operator(batch_size, n_photons),2))
+    return np.array([expm(0.5*eq[i]) for i in range(batch_size)])
 
 def D_gate(r, phi, n_photons):
     """
     Displacement gate
     """
     alpha = r*np.exp(phi*1.j)
-    eq = alpha*create_operator(n_photons) - alpha.conj()*destroy_operator(n_photons)
-    return expm(eq)
+    batch_size = alpha.shape[0]
+    alpha = alpha.reshape(batch_size, 1, 1)
+    eq = np.multiply(alpha, create_operator(batch_size, n_photons)) -\
+         np.multiply(alpha.conj(), destroy_operator(batch_size, n_photons))
+    return np.array([expm(eq[i]) for i in range(batch_size)])
 
 def X_gate(x, n_photons):
     """
     Displacement gate
     """
-    return expm((x*create_operator(n_photons) - x*destroy_operator(n_photons))/np.sqrt(2.))
+    batch_size = x.shape[0]
+    x = x.reshape(batch_size, 1, 1)
+    eq = np.multiply(x, create_operator(batch_size,n_photons) 
+           - destroy_operator(batch_size, n_photons))/np.sqrt(2.)
+    return np.array([expm(eq[i]) for i in range(batch_size)])
 
 def Z_gate(p, n_photons):
     """
     momentum displacement gate
     """
-    return expm(1.j*p*(destroy_operator(n_photons) + create_operator(n_photons))/np.sqrt(2.))
+    batch_size = p.shape[0]
+    p = p.reshape(batch_size, 1, 1)
+    eq = np.multiply(p, destroy_operator(batch_size, n_photons) +
+             create_operator(batch_size, n_photons))/np.sqrt(2.)
+    return np.array([expm(1.j*eq[i]) for i in range(batch_size)])
 
 def R_gate(theta, n_photons):
     """
     Rotation gate
     """
-    return expm(1.j*theta*create_operator(n_photons).dot(destroy_operator(n_photons)))
+    batch_size = theta.shape[0]
+    theta = theta.reshape(batch_size, 1, 1)
+    number_operator = np.einsum('ijk, ikl-> ijl', create_operator(batch_size, n_photons), 
+                      destroy_operator(batch_size, n_photons))
+    eq = np.multiply(theta, number_operator)
+    return np.array([expm(1.j*eq[i]) for i in range(batch_size)])
 
-def P_gate(s, n_photons):
+def P_gate(batch_size, n_photons):
     """
     Quadratic phase gate
+    args:
+    first batch_size !!!!!
     """
-    return expm(1.j*(destroy_operator(n_photons) + create_operator(n_photons))/4.)
+    eq = (destroy_operator(batch_size, n_photons) + create_operator(batch_size, n_photons))/4.
+    return np.array([expm(1.j*eq[i]) for i in range(batch_size)])
 
 def V_gate(gamma, n_photons):
     """
     Qubic phase gate
     """
-    return expm(1.j*gamma/3.*np.linalg.matrix_power(destroy_operator(n_photons) + create_operator(n_photons), 3)/np.sqrt(8.))
+    batch_size = gamma.shape[0]
+    gamma = gamma.reshape(batch_size, 1, 1)
+    eq = np.multiply(gamma, np.linalg.matrix_power(destroy_operator(batch_size, n_photons) +
+                create_operator(batch_size, n_photons), 3))
+    return np.array([expm(1.j*eq[i]/np.sqrt(8.)/3.) for i in range(batch_size)])
 
 def N_phase_gate(gamma, degree, n_photons):
     """
     N-phase gate
     """
-    return expm(1.j*gamma/float(degree)*np.linalg.matrix_power(destroy_operator(n_photons) + create_operator(n_photons), degree))
+    batch_size = gamma.shape[0]
+    gamma = gamma.reshape(batch_size, 1, 1)
+    eq = np.multiply(gamma, np.linalg.matrix_power(destroy_operator(batch_size, n_photons) +
+          create_operator(batch_size, n_photons), degree))
+    return np.array([expm(1.j/float(degree)*eq[i]) for i in range(batch_size)])
 
-def F_gate(n_photons):
+def F_gate(batch_size, n_photons):
     """
     Fourier gate
+    args:
+    first batch_size !!!!!
     """
-    return R_gate(np.pi/2., n_photons)
+    angles = np.zeros(batch_size)
+    angles[:] = np.pi/2.
+    angles = angles.reshape(batch_size, 1, 1)
+    return R_gate(angles, n_photons)
--- measurements.py
+++ measurements.py
@@ -4,7 +4,7 @@ from scipy.special import factorial
 from gates import R_gate
 def calc_coef(state, n_photons):
     for i in range(n_photons):
-        state[i] = state[i]*1./np.sqrt(factorial(i))*2**(-i/2.)
+        state[:, i] = state[:, i]*1./np.sqrt(factorial(i))*2**(-i/2.)
     return state
 
 def meas_X(state, n_photons):
@@ -20,17 +20,26 @@ def meas_P(state, n_photons):
     return meas_X_theta(state, np.pi/2., n_photons)
 
 def meas_X_theta(state, theta, n_photons, x_max=20, 
-                 x_min=-20, num_bins=10000, instances_size=100000):
+                 x_min=-20, num_bins=10_000, instances_size=100_000):
     """
     Measurement on X_theta quadrature
     """
+    batch_size = state.shape[0]
+    theta = theta*np.ones(batch_size)
     rotation = R_gate(-theta, n_photons)
-    state = rotation.dot(state)
+    state = np.einsum('ijk, ik -> ij', rotation, state)
+    
     x = [x_min + (x_max - x_min)/(num_bins + 1)*i for i in range(num_bins+1)]
     probs = []
     new_state = calc_coef(state, n_photons)
+    new_state = new_state.transpose(1,0)
     for x_inst in x:
         prob_x_inst = 1./np.pi**0.25*np.exp(-x_inst**2/2.)*hermval(x_inst, new_state)
         probs.append(np.abs(prob_x_inst*np.conj(prob_x_inst)))
-    probs = probs/np.sum(probs)
-    return np.random.choice(x, size=instances_size, p=probs)
+    probs = np.array(probs)
+
+    probs = probs/np.sum(probs, axis=0)
+    data = np.zeros((batch_size, instances_size))
+    for i in range(batch_size):
+        data[i] = np.random.choice(x, size=instances_size, p=probs[:,i])
+    return data
