5a7c2db1ed9bcf18da8e4abeead106249cf5808e
arsen <arsen@mail.ru>
2020-01-21 14:52:40 +0300

    bootstrapping

--- gates.py
+++ gates.py
@@ -11,76 +11,120 @@ def destroy_operator(n_photons):
 def create_operator(n_photons):
     return destroy_operator(n_photons).conj().transpose(1,0)
 
-def S_gate(r, phi, n_photons):
+def tensor_product(operation, numb_mode, n_modes, n_photons):
+    for i in range(1,n_modes+1):
+        if i==1:
+            if i==numb_mode:
+                matrix = operation
+            else:
+                matrix = np.eye(n_photons)
+        elif i==numb_mode:
+            matrix = np.kron(operation, matrix)
+        else:
+            matrix = np.kron(np.eye(n_photons), matrix)
+    return matrix
+
+
+# two mode gates 
+
+def BS_gate(theta, phi, mode1, mode2, n_modes, n_photons):
+    """
+     beamsplitter from libs
+    """
+    op1 = tensor_product(destroy_operator(n_photons), mode1, n_modes, n_photons)
+    op2 = tensor_product(destroy_operator(n_photons), mode2, n_modes, n_photons)
+    return expm(theta * (np.exp(1j * phi) * op1 @ op2.conj().T - np.exp(-1j * phi) * op1.conj().T @ op2))
+
+def CZ_gate(s, mode1, mode2, n_modes, n_photons):
+    """
+    Controlled-Z gate
+    """
+    operator = (destroy_operator(n_photons) + create_operator(n_photons))/np.sqrt(2.)
+    op1 = tensor_product(operator, mode1, n_modes, n_photons)
+    op2 = tensor_product(operator, mode2, n_modes, n_photons)
+    return expm(1j*s*op1 @ op2)
+
+def CX_gate(s, mode1, mode2, n_modes, n_photons):
+    """
+    Controlled-Z gate
+    """
+    operator_x = (destroy_operator(n_photons) + create_operator(n_photons))/np.sqrt(2.)
+    operator_p = (destroy_operator(n_photons) - create_operator(n_photons))/np.sqrt(2.)
+    op1 = tensor_product(operator_x, mode1, n_modes, n_photons)
+    op2 = tensor_product(operator_p, mode2, n_modes, n_photons)
+    return expm(s*op1 @ op2)
+
+def S_gate(r, phi, numb_mode, n_modes, n_photons):
     """
     Squeezing gate
     """
     z = r*np.exp(phi*1j)
     eq = z.conj()*np.linalg.matrix_power(destroy_operator(n_photons),2) -\
         z*np.linalg.matrix_power(create_operator(n_photons),2)
-    return expm(0.5*eq)
+    return tensor_product(expm(0.5*eq), numb_mode, n_modes, n_photons)
 
-def D_gate(r, phi, n_photons):
+def D_gate(r, phi, numb_mode, n_modes, n_photons):
     """
     Displacement gate
     """
     alpha = r*np.exp(phi*1.j)
     eq = alpha*create_operator(n_photons) -\
         alpha.conj()*destroy_operator(n_photons)
-    return expm(eq)
+    return tensor_product(expm(eq), numb_mode, n_modes, n_photons)
 
-def X_gate(x, n_photons):
+def X_gate(x, numb_mode, n_modes, n_photons):
     """
     Displacement gate on X axis
     """
     eq = (create_operator(n_photons) - destroy_operator(n_photons))/np.sqrt(2.)
-    return expm(x*eq)
+    return tensor_product(expm(x*eq), numb_mode, n_modes, n_photons)
 
-def Z_gate(p, n_photons):
+def Z_gate(p, numb_mode, n_modes, n_photons):
     """
     momentum displacement gate
     """
     eq = (destroy_operator(n_photons) +
              create_operator(n_photons))/np.sqrt(2.)
-    return expm(1.j*p*eq)
+    return tensor_product(expm(1.j*p*eq), numb_mode, n_modes, n_photons)
 
-def R_gate(theta, n_photons):
+def R_gate(theta, numb_mode, n_modes, n_photons):
     """
     Rotation gate
     """
     number_operator = np.einsum('jk, kl-> jl', create_operator(n_photons), 
                       destroy_operator(n_photons))
     eq = theta*number_operator
-    return expm(1.j*eq)
+    return tensor_product(expm(1.j*eq), numb_mode, n_modes, n_photons)
 
-def P_gate(n_photons):
+def P_gate(numb_mode, n_modes, n_photons):
     """
     Quadratic phase gate
     args:
     """
     eq = destroy_operator(n_photons) + create_operator(n_photons)
     eq = np.linalg.matrix_power(eq, 2)/4.
-    return expm(1.j*eq)
+    return tensor_product(expm(1.j*eq), numb_mode, n_modes, n_photons)
 
-def V_gate(gamma, n_photons):
+def V_gate(gamma, numb_mode, n_modes, n_photons):
     """
     Qubic phase gate
     """
     eq = gamma*np.linalg.matrix_power(destroy_operator(n_photons) +
                 create_operator(n_photons), 3)
-    return expm(1.j*eq/np.sqrt(8.)/3.)
+    return tensor_product(expm(1.j*eq/np.sqrt(8.)/3.), numb_mode, n_modes, n_photons)
 
-def N_phase_gate(gamma, degree, n_photons):
+def N_phase_gate(gamma, degree, numb_mode, n_modes, n_photons):
     """
     N-phase gate
     """
     eq = gamma*np.linalg.matrix_power(destroy_operator(n_photons) +
           create_operator(n_photons), degree)
-    return expm(1.j/float(degree)*eq)
+    return tensor_product(expm(1.j/float(degree)*eq), numb_mode, n_modes, n_photons)
 
-def F_gate(n_photons):
+def F_gate(numb_mode, n_modes, n_photons):
     """
     Fourier gate
     args:
     """
-    return R_gate(np.pi/2., n_photons)
+    return R_gate(np.pi/2., numb_mode, n_modes, n_photons)
+    
--- measurements.py
+++ measurements.py
@@ -2,33 +2,104 @@ import numpy as np
 from numpy.polynomial.hermite import hermval
 from scipy.special import factorial
 from gates import R_gate
-def calc_coef(state, n_photons):
-    for i in range(n_photons):
-        state[i] = state[i]*1./np.sqrt(factorial(i))*2**(-i/2.)
-    return state
 
-def meas_X(state, n_photons):
+
+
+def proba_1d(state, x_pret, n_photons):
+    res = 0
+    matrix = np.eye(n_photons)
+    for i in range(n_photons):
+        part = state[i]*hermval( x_pret, matrix[i])*1./np.sqrt(factorial(i))*2**(-i/2.)/np.pi**0.25*np.exp(-x_pret**2/2.)
+        res += np.abs(part*part.conj())
+    return res
+
+
+def proba_2d(state, x_pret, y_pret, n_photons):
+    matrix_factorial = np.zeros((n_photons, n_photons))
+    for i in range(n_photons):
+        for j in range(n_photons):
+                matrix_factorial[i, j] = 1./np.sqrt(factorial(i)*factorial(j))*2**(-(i+j)/2.)
+    res = 0
+    matrix = np.eye(n_photons)
+    for i in range(n_photons):
+        for j in range(n_photons):
+            part = (state[i*n_photons+j]*hermval( x_pret, matrix[i])*hermval(y_pret, matrix[j])*matrix_factorial[i, j])
+            res += np.abs(part*part.conj())
+    return res*1./np.pi*np.exp(-(x_pret**2+y_pret**2))
+
+def proba_3d(state, x_pret, y_pret, z_pret, n_photons):
+    matrix_factorial = np.zeros((n_photons, n_photons, n_photons))
+    for i in range(n_photons):
+        for j in range(n_photons):
+            for k in range(n_photons):
+                matrix_factorial[i, j, k] = 1./np.sqrt(factorial(i)*factorial(j)*factorial(k))*2**(-(i+j+k)/2.)
+    res = 0
+    matrix = np.eye(n_photons)
+    for i in range(n_photons):
+        for j in range(n_photons):
+            for k in range(n_photons):
+                part = (state[i*n_photons**2+j*n_photons+k]*hermval( x_pret, matrix[i])*hermval(y_pret, matrix[j])*
+                hermval(z_pret, matrix[k])*matrix_factorial[i, j, k])
+                res += np.abs(part*part.conj())
+    return res*1./np.pi**1.5*np.exp(-(x_pret**2+y_pret**2+z_pret**2))
+
+def bootstrap(state, n_modes, n_photons, instances_size):
+    step = 0
+    cur_length = 0
+    while cur_length <= instances_size:
+        x_pret = np.random.uniform(-5, 5, size=10000)
+        p = np.random.uniform(size=10000)
+        if n_modes==1: 
+            probabilities = proba_1d(state, x_pret, n_photons)
+        elif n_modes==2: 
+            y_pret = np.random.uniform(-5, 5, size=10000)
+            probabilities = proba_2d(state, x_pret, y_pret, n_photons)
+        elif n_modes==3: 
+            y_pret = np.random.uniform(-5, 5, size=10000)
+            z_pret = np.random.uniform(-5, 5, size=10000)
+            probabilities = proba_3d(state, x_pret, y_pret, z_pret, n_photons)
+        eq = probabilities>=p
+        length = x_pret[eq].shape[0]
+        cur_length += length
+        if n_modes==1: arr = np.asarray(x_pret[eq])
+        elif n_modes==2: arr = np.concatenate([x_pret[eq].reshape(length,1),y_pret[probabilities>=p].reshape(length,1)],axis=1)
+        elif n_modes==3: arr = np.concatenate([x_pret[eq].reshape(length,1),y_pret[probabilities>=p].reshape(length,1),
+                         z_pret[eq].reshape(length,1)],axis=1)
+        if step==0:
+            data = arr
+            step = 1
+        else:
+            data = np.concatenate([data,arr], axis=0)
+    return data[:instances_size]
+
+
+
+def meas_X(state, n_modes, n_photons):
     """
     Measurement on X quadrature
     """
-    return meas_X_theta(state, 0., n_photons)
+    return meas_X_theta(state, 0., n_modes, n_photons)
 
-def meas_P(state, n_photons):
+def meas_P(state, n_modes, n_photons):
     """
     Measurement on P quadrature
     """
-    return meas_X_theta(state, np.pi/2., n_photons)
+    return meas_X_theta(state, np.pi/2., n_modes, n_photons)
 
-def meas_X_theta(state, theta, n_photons, x_max=5, 
-                 x_min=-5, num_bins=10_000, instances_size=10_000):
+def meas_X_theta(state, theta, n_modes, n_photons, x_max=5, 
+                 x_min=-5, instances_size=10_000):
     """
     Measurement on X_theta quadrature
+    state is pure state
     """
-    rotation = R_gate(-theta, n_photons)
-    state_ = np.einsum('jk, k-> j', rotation, state)
-    x = np.linspace(x_min, x_max, num_bins+1)
-    new_state = calc_coef(state_, n_photons)
-    ampl = 1./np.pi**0.25*np.exp(-x**2/2.)*hermval(x, new_state)
-    probs = np.abs(ampl*np.conj(ampl))
-    probs /= np.sum(probs)
-    return np.random.choice(x, size=instances_size, p=probs)
+
+    # rotate to theta angle
+    for i in range(n_modes):
+        rotation = R_gate(theta, i, n_modes, n_photons)
+        state_ = rotation @ state @ rotation.conj().T
+    
+    if n_modes>3:
+        raise NotImplementedError
+    else:
+        return bootstrap(state_, n_modes, n_photons, instances_size)
+   
