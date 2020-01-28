c3ca3e0a0f3a62082ccdfbcb086d6dcbf23d508e
arsen <arsen@mail.ru>
2020-01-28 10:38:25 +0300

    simultaneous x and p

--- measurements.py
+++ measurements.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.polynomial.hermite import hermval
 from scipy.special import factorial
-from gates import R_gate
+from gates import R_gate, BS_gate
 
 
 
@@ -47,16 +47,16 @@ def bootstrap(state, n_modes, n_photons, instances_size):
     step = 0
     cur_length = 0
     while cur_length <= instances_size:
-        x_pret = np.random.uniform(-5, 5, size=10000)
-        p = np.random.uniform(size=10000)
+        x_pret = np.random.uniform(-2, 2, size=65000)
+        p = np.random.uniform(size=65000)
         if n_modes==1: 
             probabilities = proba_1d(state, x_pret, n_photons)
         elif n_modes==2: 
-            y_pret = np.random.uniform(-5, 5, size=10000)
+            y_pret = np.random.uniform(-2, 2, size=65000)
             probabilities = proba_2d(state, x_pret, y_pret, n_photons)
         elif n_modes==3: 
-            y_pret = np.random.uniform(-5, 5, size=10000)
-            z_pret = np.random.uniform(-5, 5, size=10000)
+            y_pret = np.random.uniform(-2, 2, size=65000)
+            z_pret = np.random.uniform(-2, 2, size=65000)
             probabilities = proba_3d(state, x_pret, y_pret, z_pret, n_photons)
         eq = probabilities>=p
         length = x_pret[eq].shape[0]
@@ -96,10 +96,24 @@ def meas_X_theta(state, theta, n_modes, n_photons, x_max=5,
     # rotate to theta angle
     for i in range(n_modes):
         rotation = R_gate(theta, i, n_modes, n_photons)
-        state_ = rotation @ state @ rotation.conj().T
+        state_ = rotation @ state
     
     if n_modes>3:
         raise NotImplementedError
     else:
         return bootstrap(state_, n_modes, n_photons, instances_size)
-   
+
+
+def sim_mes_xp(state, n_modes, n_photons):
+    if n_modes>1:
+        raise NotImplementedError
+    vacuum = np.zeros(n_photons)
+    vacuum[0] = 1.
+    state2d = np.kron(state, vacuum)
+    phi = np.pi/2.
+    theta = np.pi/4.
+    gate = BS_gate(theta, phi, 1, 2, 2, n_photons)
+    state2d_ = np.einsum('jk,k->j', gate, state2d)
+    gate2 = R_gate(-np.pi/2, 1, 2, n_photons)
+    state2d_final = np.einsum('jk,k->j', gate2, state2d_)
+    return meas_X_theta(state2d_final, 0, 2, n_photons)
